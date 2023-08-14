c8a9577dd3041bd70a5494d237fe3ad8e8249c49
Ng'ash_254 <samuelnganga.w@gmail.com>
2023-08-14 15:55:04 +0300

    Add API fetch separately

---
+++ src/fetchPet.jsx
@@ -0,0 +1,12 @@
+const fetchPet = async ({ queryKey }) => {
+	const id = queryKey[1];
+	const apiRes = await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`);
+
+	if (!apiRes) {
+		throw new Error(`details/${id} fetch is not ok`);
+	}
+
+	return apiRes;
+};
+
+export default fetchPet;
