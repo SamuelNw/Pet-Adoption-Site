189f14267673afacf0e5206c607d9ea82113cc12
Ng'ash_254 <samuelnganga.w@gmail.com>
2023-08-14 15:09:41 +0300

    Declutter

--- src/Details.jsx
+++ src/Details.jsx
@@ -2,11 +2,7 @@ import { useParams } from "react-router-dom";
 
 const Details = () => {
 	const { id } = useParams();
-	return (
-		<div>
-			<h1>{id}</h1>
-		</div>
-	);
+	return <h1>{id}</h1>;
 };
 
 export default Details;
